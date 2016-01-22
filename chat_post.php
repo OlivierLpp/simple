0c988b1cc814615c60c242692022cdef6102d83d
olivier <o.lepestipon@gmail.com>
2016-01-22 16:15:46 +0100

    ajout de la lecture base de donnée dans chat_post.php

--- chat_post.php
+++ chat_post.php
@@ -11,8 +11,21 @@ session_start();
     </head>
 <body>	
 
+<?php
+// Effectuer ici la requête qui insère le message
+// Puis rediriger vers minichat.php comme ceci :
 
+try
+{
+    // On se connecte à MySQL
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
 
-
+?>
  	 </body>
 </html>
